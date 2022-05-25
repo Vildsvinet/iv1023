b63a81ce96a5204af3de1eeb08b21d2aa497a141
Tove <95482627+Vildsvinet@users.noreply.github.com>
2022-05-25 13:40:58 +0200

    Update 4uppg4.sql
    final

--- inlupp4/4uppg4.sql
+++ inlupp4/4uppg4.sql
@@ -1,4 +1,6 @@
---Ta fram namn och land på författare till böcker som har översatts till ryska! Resultatet skall ha två kolumner
+/*Inlupp 4.4
+  Ta fram namn och land på författare till böcker som har översatts till ryska! Resultatet skall ha två kolumner
+*/
 
 SELECT Author.name, ll.land
 FROM Edition, Book, Author, Authorship,
@@ -15,24 +17,24 @@ FROM Edition, Book, Author, Authorship,
 WHERE tt.Språk = 'Russian' AND book.id = edition.book
   AND book.id = authorship.book AND author.id = authorship.author
 GROUP BY Author.name, ll.land
-
+ORDER BY Author.name
 
 /*
-    NAME                	LAND
- ------------------- 	--------
- Andreas Shultz     	Austria
- Alicia Bing        	Belgium
- Pierre Zargone     	Belgium
- John Craft         	England
- Peter Feldon       	England
- Carl George        	France
- Antje Liedderman   	Germany
- Kostas Andrianos   	Greece
- Sam Davis          	Mexiko
- Christina Ohlsen   	Norway
- Auna Gonzales Perre	Portugal
- Lilian Carrera     	Spain
- Carl Sagan         	USA
- Linda Evans        	USA
- Mimi Pappas        	USA
+ NAME                LAND
+ ------------------- --------
+ Alicia Bing         Belgium
+ Andreas Shultz      Austria
+ Antje Liedderman    Germany
+ Auna Gonzales Perre Portugal
+ Carl George         France
+ Carl Sagan          USA
+ Christina Ohlsen    Norway
+ John Craft          England
+ Kostas Andrianos    Greece
+ Lilian Carrera      Spain
+ Linda Evans         USA
+ Mimi Pappas         USA
+ Peter Feldon        England
+ Pierre Zargone      Belgium
+ Sam Davis           Mexiko
 */
