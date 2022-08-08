5860acb8c71f36aacb9f159a5c39eac5d6ed766f
Tove <95482627+Vildsvinet@users.noreply.github.com>
2022-08-09 01:03:26 +0200

    8.3

--- inlupp8/8uppg3.sql
+++ inlupp8/8uppg3.sql
@@ -1,3 +1,116 @@
+---------------------augusti versioner
+SELECT XMLELEMENT(NAME "Resultat", XMLAGG(tst))
+FROM
+    (
+        SELECT XMLELEMENT(NAME "Genre", XMLATTRIBUTES(genre as "namn"), spraaak) AS tst
+        FROM (
+                 SELECT genre, XMLAGG(XMLELEMENT(NAME "Språk", språk)) as spraaak
+                 FROM (
+                          select distinct genre, t1.språk as språk
+                          from book,
+                               (select book, language as språk
+                                from edition, xmltable('$t//Translation'
+                                                       PASSING translations as "t"
+                                                       COLUMNS language VARCHAR(20) PATH '@Language'
+                                    )
+                               ) t1
+                          WHERE book.id = t1.book
+                          ORDER BY genre
+                      )
+                 group by genre
+             )
+    )
+
+        SELECT XMLELEMENT(NAME "Genre", XMLATTRIBUTES(genre as "namn"), spraaak) AS tst
+FROM (
+    SELECT genre, XMLAGG(XMLELEMENT(NAME "Språk", språk)) as spraaak
+    FROM (
+    select distinct genre, t1.språk as språk
+    from book,
+    (select book, language as språk
+    from edition, xmltable('$t//Translation'
+    PASSING translations as "t"
+    COLUMNS language VARCHAR(20) PATH '@Language'
+    )
+    ) t1
+    WHERE book.id = t1.book
+    ORDER BY genre
+    )
+    group by genre
+    )
+
+SELECT XMLELEMENT(NAME "Resultat", XMLAGG(tst))
+FROM
+    (
+        SELECT XMLELEMENT(NAME "Genre", XMLATTRIBUTES(genre as "namn"), ogl, spraaak) AS tst
+        FROM (
+
+                 SELECT genre, XMLAGG(XMLELEMENT(NAME "Språk", språk)) as spraaak, XMLAGG(XMLELEMENT(NAME "Språk", originallanguage)) as ogl
+                 FROM (
+
+                          SELECT distinct genre, originallanguage, language as språk
+                          FROM book,
+                               (select book, language
+                                from edition, xmltable('$t//Translation'
+                                                       PASSING translations as "t"
+                                                       COLUMNS language VARCHAR(20) PATH '@Language'
+                                    )
+                               ) t1
+                          WHERE book.id = t1.book
+                            AND genre IS NOT NULL
+                          ORDER BY genre, språk
+
+                      )
+                 GROUP BY genre
+
+             )
+    )
+
+
+        SELECT genre,
+
+XMLAGG(XMLELEMENT(NAME "Sprok",
+(SELECT originallanguage FROM (SELECT distinct genre, originallanguage, language as språk
+	  FROM book,
+	       (select book, language
+	        from edition, xmltable('$t//Translation'
+	                           PASSING translations as "t"
+	                           COLUMNS language VARCHAR(20) PATH '@Language'
+	            )
+	       ) t1
+	  WHERE book.id = t1.book
+	  	AND genre IS NOT NULL
+	  ORDER BY genre, språk ) UNION SELECT språk FROM (SELECT distinct genre, originallanguage, language as språk
+	  FROM book,
+	       (select book, language
+	        from edition, xmltable('$t//Translation'
+	                           PASSING translations as "t"
+	                           COLUMNS language VARCHAR(20) PATH '@Language'
+	            )
+	       ) t1
+	  WHERE book.id = t1.book
+	  	AND genre IS NOT NULL
+	  ORDER BY genre, språk ))
+
+)) as språken
+
+
+
+FROM (
+    SELECT distinct genre, originallanguage, language as språk
+    FROM book,
+    (select book, language
+    from edition, xmltable('$t//Translation'
+    PASSING translations as "t"
+    COLUMNS language VARCHAR(20) PATH '@Language'
+    )
+    ) t1
+    WHERE book.id = t1.book
+    AND genre IS NOT NULL
+    ORDER BY genre, språk ) t2
+GROUP BY genre
+
+--------------------------------------------------------------
 --SELECT COUNT(t1.bks)
 --FROM  (SELECT book.id AS bks FROM book WHERE book.genre = 'Novel') t1
 
