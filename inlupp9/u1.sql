bfc8189219b50714180c996601f04b2be246ae02
Tove <95482627+Vildsvinet@users.noreply.github.com>
2022-08-22 00:48:37 +0200

    Update u1.sql
    first db2 version

--- inlupp9/u1.sql
+++ inlupp9/u1.sql
@@ -1,39 +1,43 @@
+-- Inlupp 9.1 db2
 --  Vem äger bilar i varje färg? Resultatet skall ha följande struktur:
--- <Resultat>
--- <Färg namn="">
--- <Person namn="" antalarbetsgivare="" antalanställningar="" />
--- <Person namn="" antalarbetsgivare="" antalanställningar="" />
--- </Färg>
--- <Färg namn="">
--- <Person namn="" antalarbetsgivare="" antalanställningar="" />
--- <Person namn="" antalarbetsgivare="" antalanställningar="" />
--- </Färg>
--- </Resultat>
+
 
 SELECT XMLELEMENT(NAME "Resultat", XMLAGG(colours))
 FROM (
          SELECT XMLELEMENT(NAME "Färg", XMLATTRIBUTES(color AS "namn"),
-                           XMLAGG(XMLELEMENT(NAME "Person", name))) AS colours
+                           XMLAGG(XMLELEMENT(NAME "Person",
+                                   XMLATTRIBUTES(cart.NAME AS "namn", empt.antalarbetsgivare AS "antalarbetsgivare",
+                                                 empt.antalanställningar AS "antalanställningar")))) AS colours
          FROM (SELECT DISTINCT COLOR, OWNER, NAME
-               FROM CAR,
-                    PERSON
-               WHERE CAR.OWNER = PERSON.PID)
-            ,
-              (SELECT EMPLOYMENTS FROM PERSON)
-            ,
-              XMLTABLE('$e//Employment'
-                  PASSING employments AS "e"
-                  COLUMNS arbetsgivare VARCHAR(10) PATH '@employer')
+               FROM car,
+                    person
+               WHERE CAR.OWNER = PERSON.PID) cart,
+              (SELECT name, antalarbetsgivare AS antalarbetsgivare, anstallningar AS antalanställningar
+               FROM person, XMLTABLE('$e'
+                                     PASSING employments AS "e"
+                                     COLUMNS
+                                     antalarbetsgivare INTEGER PATH 'count(distinct-values(//@employer))',
+                                     anstallningar INTEGER PATH 'count(.//employment)')) empt
+         WHERE empt.NAME = cart.NAME
          GROUP BY COLOR
-)
+     );
 
-
-SELECT name, antalarbetsgivare AS antalarbetsgivare, anstallningar AS antalanställningar
-
-FROM       person, XMLTABLE('$e'
-                  PASSING employments AS "e"
-                  COLUMNS
-                    antalarbetsgivare INTEGER PATH 'count(distinct-values(//@employer))',
-                    anstallningar XML PATH 'count($e//employment)')
-
-                  ;
+/*
+<Resultat>
+    <Färg namn="black">
+        <Person namn="John Higgins" antalarbetsgivare="2" antalanställningar="2"/>
+        <Person namn="Ronnie O'Sullivan" antalarbetsgivare="2" antalanställningar="2"/>
+    </Färg>
+    <Färg namn="blue">
+        <Person namn="Stephen Hendry" antalarbetsgivare="2" antalanställningar="3"/>
+        <Person namn="Ken Doherty" antalarbetsgivare="4" antalanställningar="5"/>
+    </Färg>
+    <Färg namn="green">
+        <Person namn="Matthew Stevens" antalarbetsgivare="1" antalanställningar="1"/>
+    </Färg>
+    <Färg namn="red">
+        <Person namn="Matthew Stevens" antalarbetsgivare="1" antalanställningar="1"/>
+        <Person namn="Steve Davis" antalarbetsgivare="3" antalanställningar="3"/>
+    </Färg>
+</Resultat>
+*/
