27fe170211868919800dc57ce7ce352de8d1a57b
Tove <95482627+Vildsvinet@users.noreply.github.com>
2022-08-23 18:52:33 +0200

    Update u3oracle.sql

--- inlupp9/u3oracle.sql
+++ inlupp9/u3oracle.sql
@@ -1,8 +1,93 @@
-SELECT person.NAME, arbetsgivare AS företag,
-       XMLELEMENT(NAME "Märke", XMLATTRIBUTES(brand AS "namn"))
-FROM CAR JOIN PERSON ON CAR.OWNER = PERSON.PID,
-     XMLTABLE('$e//employment'
-         PASSING employments AS "e"
-         COLUMNS
-         arbetsgivare VARCHAR(10) PATH '@employer'
+
+SELECT XMLELEMENT(NAME "Märke",
+                XMLATTRIBUTES(brand AS "namn"),
+                XMLAGG( XMLELEMENT(NAME "Företag",
+                        XMLATTRIBUTES(arbetsgivare AS "namn"
+                            ,
+                            (
+                                SELECT COUNT(DISTINCT (PERSON.NAME)) as antal FROM CAR JOIN PERSON ON CAR.OWNER = PERSON.PID,
+                                    XMLTABLE('$e//employment'
+                                             PASSING employments AS "e"
+                                             COLUMNS
+                                             arbetsgivare VARCHAR(10) PATH '@employer'
+                                    )
+                                WHERE brand = yttre.BRAND AND arbetsgivare = yttre.arbetsgivare
+                                GROUP BY arbetsgivare
+                                )
+                             AS "antalbilägare"
+                        )
+                    ))
+                )
+
+FROM (
+    SELECT distinct arbetsgivare, brand
+    FROM CAR JOIN PERSON ON CAR.OWNER = PERSON.PID,
+        XMLTABLE('$e//employment'
+               PASSING employments AS "e"
+               COLUMNS
+               arbetsgivare VARCHAR(10) PATH '@employer'
         )
+    ) yttre
+GROUP BY BRAND
+
+
+;SELECT XMLELEMENT(NAME "Märke",
+                XMLATTRIBUTES(brand AS "namn"),
+                XMLAGG( XMLELEMENT(NAME "Företag",
+                        XMLATTRIBUTES(arbetsgivare AS "namn"
+                            ,
+                            (
+                            SELECT antal
+                            FROM (
+                                SELECT arbetsgivare, COUNT(DISTINCT (PERSON.NAME)) as antal FROM CAR JOIN PERSON ON CAR.OWNER = PERSON.PID,
+                                    XMLTABLE('$e//employment'
+                                             PASSING employments AS "e"
+                                             COLUMNS
+                                             arbetsgivare VARCHAR(10) PATH '@employer'
+                                    )
+                                WHERE brand = yttre.BRAND AND arbetsgivare = yttre.arbetsgivare
+                                GROUP BY arbetsgivare
+                                )
+                            ) AS "antalbilägare"
+                        ))
+                )
+    )
+FROM (
+    SELECT distinct arbetsgivare, brand
+    FROM CAR JOIN PERSON ON CAR.OWNER = PERSON.PID,
+        XMLTABLE('$e//employment'
+               PASSING employments AS "e"
+               COLUMNS
+               arbetsgivare VARCHAR(10) PATH '@employer'
+        )
+    ) yttre
+GROUP BY BRAND
+;
+
+
+
+
+
+SELECT arbetsgivare AS företag, person.NAME,
+       XMLELEMENT(NAME "Märke", XMLATTRIBUTES(brand AS "namn"))
+FROM CAR
+         JOIN PERSON ON CAR.OWNER = PERSON.PID,
+    XMLTABLE('$e//employment'
+             PASSING employments AS "e"
+             COLUMNS
+             arbetsgivare VARCHAR(10) PATH '@employer'
+        )
+ORDER BY företag
+;
+
+SELECT arbetsgivare, COUNT(DISTINCT (PERSON.NAME)) as antal
+FROM CAR
+         JOIN PERSON ON CAR.OWNER = PERSON.PID,
+    XMLTABLE('$e//employment'
+             PASSING employments AS "e"
+             COLUMNS
+             arbetsgivare VARCHAR(10) PATH '@employer'
+        )
+WHERE brand = 'FIAT'
+group by arbetsgivare
+;
